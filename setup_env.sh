17df7cd5c23f998f5ba3ccc623f3242409702645
nehabansaliit <nehabansaliit@gmail.com>
2026-02-03 10:26:35 -0700

    Updated setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,16 @@
-npm install
+#!/bin/bash
+set -e
+
+echo "Node version:"
+node -v
+npm -v
+
+echo "Installing project dependencies"
+npm ci
+
+echo "Installing Allure"
 npm install -D allure-playwright
 npm install -g allure-commandline --force
+
+echo "Installing Playwright browsers (chromium + firefox)"
 npx playwright install chromium firefox
